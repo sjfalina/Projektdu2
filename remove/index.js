835f5dae1ef1a5b370ae4ff4c21194d44011d6b8
sjfalina <alinab91@hotmail.com>
2024-12-21 19:12:16 +0100

    Made the New random number-button useful
    Now when the button is clicked a random number is generated and if the number matches any of the number-divs the boxes will turn orange.

--- remove/index.js
+++ remove/index.js
@@ -11,12 +11,24 @@ newRandomButton.textContent = "New random number";
 removeCreator.appendChild(newRandomButton);
 const newInput = document.createElement("input");
 newInput.style.marginRight = "5px";
+newInput.placeholder = "-";
 removeCreator.appendChild(newInput);
 const removeButton = document.createElement("button");
 removeButton.textContent = "Remove";
 removeCreator.appendChild(removeButton);
 const removeInput = document.createElement("input");
+removeInput.placeholder = "-";
 removeCreator.appendChild(removeInput);
 
 createRandomNumbers(howInput.value);
 
+newRandomButton.addEventListener("click", function () {
+    newInput.value = getRandomNumber();
+
+    for (let div of numbers.childNodes) {
+        div.classList.remove("mark");
+        if (div.textContent == newInput.value) {
+            div.classList.add("mark")
+        }
+    }
+})
