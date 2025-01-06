e407c49309caa5e5f62368b30c0c285bada23490
sjfalina <alinab91@hotmail.com>
2025-01-06 10:33:27 +0100

    Now it is clear which numbers are repeated

--- repetition/index.js
+++ repetition/index.js
@@ -2,6 +2,7 @@ createControlElements();
 createRandomNumbers(howInput.value);
 
 function checkRepeats() {
+
     let repeatedNumbers = [];
     let repeats = 0;
     for (let i = 1; i < 100; i++) {
@@ -19,5 +20,31 @@ function checkRepeats() {
         }
 
     }
-    return repeatedNumbers;
+    return { repeatedNumbers, repeats }
+}
+
+const mostRepsText = document.createElement("p");
+mostRepsText.textContent = "Most repeated number(s):";
+mostRepsText.style.display = "inline";
+creator.appendChild(mostRepsText);
+const mostReps = document.createElement("div");
+mostReps.style.width = "200px";
+mostReps.style.height = "50px";
+mostReps.style.padding = "8px";
+mostReps.style.margin = "3px";
+mostReps.style.border = "1px solid black";
+mostReps.style.display = "inline";
+creator.appendChild(mostReps);
+const result = checkRepeats();
+
+if (result.repeats > 1) {
+    const output = `${result.repeatedNumbers.join(", ")} (Repeated ${result.repeats} times)`;
+    mostReps.textContent = output;
+    for (let number of result.repeatedNumbers) {
+        for (let node of numbers.childNodes) {
+            if (number == node.textContent) {
+                node.classList.add("mark");
+            }
+        }
+    }
 }
