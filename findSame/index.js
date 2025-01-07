a3af05cb8566a0aa5c031d04fa32e425da884ec4
sjfalina <alinab91@hotmail.com>
2025-01-07 09:15:57 +0100

    Some basic things for this page
    Info about what the page does and made the numbers clickable which also finds copies of the same number...
    Made a resetbutton too.

--- findSame/index.js
+++ findSame/index.js
@@ -1,2 +1,28 @@
 createControlElements();
 createRandomNumbers(howInput.value);
+
+const findSameDiv = document.createElement("div");
+creator.appendChild(findSameDiv);
+const findText = document.createElement("p");
+findSameDiv.appendChild(findText);
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+findSameDiv.appendChild(resetButton);
+
+const startMessage = "Click on a number to find copies";
+findText.textContent = startMessage;
+
+numbers.addEventListener("click", function (e) {
+    if (e.target.className == "numberDiv") {
+        e.target.classList.add("mark");
+        for (let node of numbers.childNodes) {
+            if (node.textContent == e.target.textContent) {
+                node.classList.add("mark");
+            } else {
+                node.classList.remove("mark");
+            }
+        }
+    }
+}
+)
+
