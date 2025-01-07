f8c24cce2efb1e2a3376a20b3be87616a3a918a8
sjfalina <alinab91@hotmail.com>
2025-01-07 09:17:20 +0100

    Added function to the reset-button
    And added a text message about how many copies there are of the certain number clicked.

--- findSame/index.js
+++ findSame/index.js
@@ -15,14 +15,29 @@ findText.textContent = startMessage;
 numbers.addEventListener("click", function (e) {
     if (e.target.className == "numberDiv") {
         e.target.classList.add("mark");
+        let counter = 1;
         for (let node of numbers.childNodes) {
             if (node.textContent == e.target.textContent) {
                 node.classList.add("mark");
+                counter++;
             } else {
                 node.classList.remove("mark");
             }
+        }
+        if (counter == 1) {
+            findText.textContent = `${counter} copy of ${e.target.textContent}`;
+        } else {
+            findText.textContent = `${counter} copies of ${e.target.textContent}`;
         }
     }
 }
 )
 
+resetButton.addEventListener("click", function () {
+    for (let node of numbers.childNodes) {
+        if (node.classList.contains("mark")) {
+            node.classList.remove("mark");
+        }
+    }
+    findText.textContent = startMessage;
+})
