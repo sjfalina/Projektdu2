ee84955a9bf57ee59c2ee424e203b24fb7fbc701
sjfalina <alinab91@hotmail.com>
2025-01-08 16:27:26 +0100

    Got some hover-events for the numbers
    If you hover over an unmarked numberbox it turns blue, and if you click on it the number is cleared and the box should turn orange. If you then click on an orange, empty box it is "refilled" with a new random number.

--- clear/index.js
+++ clear/index.js
@@ -2,18 +2,35 @@ const fillButton = document.createElement("button");
 fillButton.textContent = "Fill cleared";
 creator.appendChild(fillButton);
 
+numbers.addEventListener("mouseover", function (e) {
+    if (e.target.className == "numberDiv") {
+        e.target.style.backgroundColor = "skyblue";
+    } else if (e.target.className.includes("markRed")) {
+        e.target.style.backgroundColor = "orange";
+    }
+})
+
+numbers.addEventListener("mouseout", function (e) {
+    e.target.style.backgroundColor = "";
+})
+
 numbers.addEventListener("click", function (e) {
     if (e.target.className == "numberDiv") {
-        e.target.classList.add("mark");
+        e.target.classList.add("markRed");
         e.target.textContent = "";
+    } else if (e.target.className.includes("markRed")) {
+        e.target.textContent = getRandomNumber();
+        e.target.classList.remove("markRed");
     }
 });
 
+
+
 fillButton.addEventListener("click", function () {
     for (let div of numbers.childNodes) {
-        if (div.className.includes("mark")) {
+        if (div.className.includes("markRed")) {
             div.textContent = getRandomNumber();
-            div.classList.remove("mark");
+            div.classList.remove("markRed");
         }
     }
 
